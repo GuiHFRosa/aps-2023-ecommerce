cb05f6bf4d54cfe889935a8e6ae812b8fdf723af
lunadevrosa <gui.player@hotmail.com>
2023-05-17 22:05:47 -0300

    setup home screen shape and create basic product crud

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        'macos-ventura': "url('macos-ventura.jpg')",
+      }
     },
   },
   plugins: [
