61d4e94f7cb3a9ed88a84093a80fa2870f9785f7
lunadevrosa <gui.player@hotmail.com>
2023-05-22 22:03:20 -0300

    basic implementation of cart page, standardize cart and catalog

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,17 +1,21 @@
 class CartController < ApplicationController
   def index
     @cart = get_cart
+    puts @cart
   end
 
-  def add_cart_item(product_id)
+  def add_cart_item
+    product_id = params[:product_id]
     @cart = get_cart
     @cart.cart_items.find { |cart_item| cart_item.product_id == product_id  }
-    @cart_item = CartItem.new(:cart_id => @cart.id, :product_id => product_id, :quantity => 1)
+    @cart.cart_items.build(:cart_id => @cart.id, :product_id => product_id, :quantity => 1)
+    @cart.save
   end
 
   private
   def get_cart
     @cart = Cart.includes(:cart_items).find_by(user_id: current_user.id)
     @cart = Cart.new(:user_id => current_user.id) unless @cart
+    @cart
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     root 'home#index'
     get '/catalog', to: 'catalog#index'
     get '/cart', to: 'cart#index'
+    post '/cart/cart_item', to: 'cart#add_cart_item'
   end
 end
